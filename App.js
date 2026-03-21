970b517f5904d8d3f1e5d94465c457613b6c8884
Neha <neha.contact295@gmail.com>
2026-03-21 16:57:31 +0530

    episode-03

--- App.js
+++ App.js
@@ -107,27 +107,201 @@
 
 // ***Episode 4 - Part 2***  
 
-import React from "react"; //coming from react modules
-// import ReactDOM from "react-dom";
-import ReactDOM from "react-dom/client"; // to remove warning 
-// but these imports are not normal JS
-// so we need to tell that it isn't a normal JS file , but a module , write in index.html script
+// import React from "react"; //coming from react modules
+// // import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom/client"; // to remove warning 
+// // but these imports are not normal JS
+// // so we need to tell that it isn't a normal JS file , but a module , write in index.html script
 
-// now as soon as we save the file , our browser also automatically refresh
+// // now as soon as we save the file , our browser also automatically refresh
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "Neha in this tag"),
-        React.createElement("h2", {}, "I'm h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I'm h1 tag"),
-        React.createElement("h2", {}, "I'm h2 tag"),
-    ]),
-]);
+// const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     [React.createElement("div", { id: "child" }, [
+//         React.createElement("h1", {}, "Neha in this tag"),
+//         React.createElement("h2", {}, "I'm h2 tag"),
+//     ]),
+//     React.createElement("div", { id: "child2" }, [
+//         React.createElement("h1", {}, "I'm h1 tag"),
+//         React.createElement("h2", {}, "I'm h2 tag"),
+//     ]),
+// ]);
 
-console.log(parent);
+// console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+
+
+
+// ***Episode 3 - Part 2***
+// let's build from scratch
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element === DOM element
+// it is an object
+// when we render it onto DOM, it becomes an HTML
+// React.createElement => ReactElement(JS Object) => HTMLElement(render)
+// const heading = React.createElement(
+//     "h1",
+//     { id: "heading" },
+//     "Namaste React"
+// );
+// // this is core of react, creating React element
+// console.log(heading);
+
+// // const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// // root.render(heading);
+
+
+
+
+// ***Episode 3 - Part 3***
+// React.createElement, it is clumsy, it's not a good way , not dev friendly
+// JSX was developed -> JS syntax 
+// it isn't part of react
+// react is diff & jsx is diff, react can be written without jsx too, but jsx makes life easy
+
+// jsx != html inside JS
+// jsx is HTML-like or XML-like syntax
+// const jsxHeading = <h1 id="heading" className="head">Namaste React using JSX</h1>
+// console.log(jsxHeading);
+
+// heading & jsxHeading are both same objects
+
+// jsxHeading is the valid JS 
+// jsxHeading isn't the valid pure JS as JS engine can't understand/read it
+// but parcel is doing the job BTS
+// when the code is wirtten, even before it goes to JS engine, it is transpiled(React.createElement) before & then JS engine gets the code which browser understands
+// JSX (transpiled before it reaches JS Engine) - PARCEL - Babel(JS Compiler/Transpiler)
+
+// how is the code running?
+// JSX - Babel transpiles it to => React.createElement => ReactElement(JS Object) => HTMLElement(render)
+// JSX != HTML , it is HTML-like
+// as in HTML it is class, in JSX it is className
+// JSX use "camelCase" in attributes
+// H.W how to wirte an inage tag, anchor tag inside JSX
+
+// writitng in multiple lines need wrapping with "(, )" bz babel needs to know where JSX starts & ends
+// const jsxHeading = (
+//     <h1 id="heading" className="head">
+//         Namaste React using JSX
+//     </h1>
+// );
+// // use prettier extension
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+
+
+
+
+
+
+// ***Episode 3 - Part 4***
+
+// React Element - JSX
+const heading = (
+    <h1 id="heading" className="head" tabIndex="5">
+        Namaste React using JSX
+    </h1>
+);
+
+// React Component - evrything is a comp in React(head, search , fotter, list of cards, evrything)
+// what's a comp? 2 types of comp in react
+// 1. Class Based Components - OLD - uses JS classes
+// 2. Functional Components - NEW - uses JS Fxn to create comp 
+
+// React functional comp: just a normal JS fxn, which returns some JSX(React Elt)
+const HeadingComponent = () => {
+    return <h1 className="heading">Namaste React Functional Component</h1>;
+};
+// or
+const HeadingComponent2 = () => <h1 className="heading">Namaste React Functional Component</h1>;
+// or
+// common syntax
+const HeadingComponent3 = () => (
+    <div id="dabba">
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+);
+// all 3 r same
+
+// if single line, return keyword x , on same fn line
+// const fn = () => true;
+// or 
+// const fn = () => {
+//     return true;
+// };
+
+// react elt => react func comp : adding array fxn syntax to react elt & making name initials capital
+
+// const Title = function () {
+//     return (
+//         <h1 className="head" tabIndex="5">
+//             Namaste React using JSX - Title
+//         </h1>
+//     );
+// };
+// or
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        Namaste React using JSX - Title
+    </h1>
+);
+
+// react elt inside react elt
+const elem = <span>React Element</span>
+const title = (
+    <h1 className="head">
+        {elem}
+        Namaste React using JSX - elt
+    </h1>
+);
+
+const num = 10000;
+
+// ***cross site scripting - can steal cookie, localStorage, etc, etc
+// const data= api.getData();
+
+const HeadingComponentt = () => (
+    <div id="dabba">
+        {/* <Title /> */}
+        {/* this is called component compositions: FC inside FC*/}
+        {/* or */}
+        {/* <Title></Title> */}
+        {/* or */}
+        {Title()}        
+        {/* all 3 r same */}
+
+        {/* right now we r inside jsx btwn "(....JSX....);" -> we can write any JS code inside {} */}
+        {/* {num} */}
+        <h1>{num}</h1>
+        <h1>{console.log("Neha")}</h1>
+        {12+21}
+
+        {/* react elt inside FC */}
+        {title}
+
+        {/* {data} */}
+        {/* but JSX is so amazing that it takes care of this kinda injection attacks, JSX escapes it , JSX sanitizes it and then pass 
+        H.W. read more about it*/}
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading); // render react elt   
+
+// how to render react func comp?
+root.render(<HeadingComponentt />)
+
+
+
+
+
+
+// ***Episode 3 - Part 5***
---
+++ episode3.md
@@ -0,0 +1,15 @@
+<!-- ***Episode 3 - Part 1***  -->
+<!-- 
+to make ervything easy , to run our project app , instead of running this whole command
+npx parcel index.html -> we'll create a script that will build our project
+create script in package.json in scripts i.e. script for dev("start": "parcel index.html"), prod("build": "parcel build index.html")
+standard convention in industry
+so now do "npm run name_of_script" -> npm run start(or npm start -> only for start), npm run build(not npm build)
+project will start on dev server
+ -->
+
+
+
+
+<!-- ***Episode 3 - Part 2***  -->
+<!-- Let's start coding -->
